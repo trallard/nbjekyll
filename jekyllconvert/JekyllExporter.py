f7e56939a77cfaa33c1701aad3c4c34081984923
Tania Allard <t.allard@sheffield.ac.uk>
2017-12-13 14:11:05 +0000

    Update exporter and templates

--- jekyllconvert/JekyllExporter.py
+++ jekyllconvert/JekyllExporter.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Custom Markdown template for Jekyll purposes
-Adapted from the HTML / markdown Exporters
-Distributed under the terms of the Modified BSD License.
-
-"""
-
+# file __init__.py
 import os
-import path
+import os.path
+
 
 from traitlets import default, Unicode
 from traitlets.config import Config
@@ -18,35 +12,41 @@ from nbconvert.filters.markdown_mistune import IPythonRenderer, MarkdownWithMath
 from nbconvert.exporters.html import HTMLExporter
 
 
+
+#-----------------------------------------------------------------------------
+# Classes
+#-----------------------------------------------------------------------------
+
 class JekyllExporter(HTMLExporter):
     """
-    Exports a basic HTML document.  This exporter assists with the export of
-    HTML.  Inherit from it if you are writing your own HTML template and need
-    custom preprocessors/filters.  If you don't need custom preprocessors/
-    filters, just change the 'template_file' config option.
+    My custom exporter
     """
 
     anchor_link_text = Unicode(u'¶', help="The text used as the text for anchor links.").tag(config=True)
-
     output_mimetype = 'text/markdown'
 
     def _file_extension_default(self):
-        """The exporter will return a .md file"""
+        """
+        The new file extension is `.test_ext`
+        """
         return '.md'
 
     @property
     def template_path(self):
         """
-        We want to inherit from HTML template, and have  the template under
+        We want to inherit from HTML template, and have template under
         `./templates/` so append it to the search path. (see next section)
         """
-        return super().template_path+[os.path.join(os.path.dirname(__file__), "templates")]
+        #return super().template_path+[os.path.join(os.path.dirname(__file__), "templates")]
+        module_path = os.path.join(os.path.dirname(__file__), "templates")
+        print(module_path)
+        return super().template_path + [module_path]
 
     def _template_file_default(self):
         """
         We want to use the new template we ship with our library.
         """
-        return  'jekyll_html'
+        return 'test_template'
 
     @property
     def default_config(self):
@@ -74,20 +74,3 @@ class JekyllExporter(HTMLExporter):
         c.merge(super(HTMLExporter, self).default_config)
         return c
 
-    def markdown2html(self, source):
-        """Markdown to HTML filter respecting the anchor_link_text setting"""
-        renderer = IPythonRenderer(escape=False,
-                                   anchor_link_text=self.anchor_link_text)
-        return MarkdownWithMath(renderer=renderer).render(source)
-
-    def default_filters(self):
-        for pair in super(HTMLExporter, self).default_filters():
-            yield pair
-        yield ('markdown2html', self.markdown2html)
-
-    def from_notebook_node(self, nb, resources=None, **kw):
-        langinfo = nb.metadata.get('language_info', {})
-        lexer = langinfo.get('pygments_lexer', langinfo.get('name', None))
-        self.register_filter('highlight_code',
-                             Highlight2HTML(pygments_lexer=lexer, parent=self))
-        return super(HTMLExporter, self).from_notebook_node(nb, resources, **kw)
--- jekyllconvert/templates/test_template.tpl
+++ jekyllconvert/templates/test_template.tpl
@@ -1,11 +1,89 @@
-{% extends "full.tpl" %}
+{% extends "basic.tpl" %}
 
-{% block markdowncell -%}
+{# custom header for jekyll post #}
+{%- block header -%}
+---
+layout: notebook
+title: "{{resources['metadata']['name']}}"
+tags:
+update_date:
+code_version: 1
+validation_pass:
+---
+<br />
+{%- endblock header -%}
 
+{% block in_prompt -%}
+<div class="prompt input_prompt">
+{%- if cell.execution_count is defined -%}
+{%- if resources.global_content_filter.include_input_prompt-%}
+<font color ='#00bcd4'>In&nbsp;[{{ cell.execution_count|replace(None, "&nbsp;") }}]: </font>
+{%- else -%}
+In&nbsp;[&nbsp;]:
+{%- endif -%}
+{%- endif -%}
+</div>
+{%- endblock in_prompt %}
 
-## this is a markdown cell
-{super()}
-## THIS IS THE END
+{% block data_markdown scoped %}
+{{ output.data['text/markdown'] }}
+{% endblock data_markdown %}
 
-
+{% block markdowncell scoped %}
+{{ cell.source }}
 {% endblock markdowncell %}
+
+{% block data_html scoped -%}
+{{ output.data['text/markdown'] | markdown2html }}
+{%- endblock data_html %}
+
+{# Images handling#}
+{% block data_svg %}
+{%- if output.svg_filename %}
+<img src="{{ output.svg_filename | posix_path }}" alt="svg">
+{%- else %}
+{{ output.data['image/svg+xml'] }}
+{%- endif %}
+{%- endblock data_svg %}
+
+{% block data_png scoped %}
+{%- if 'image/png' in output.metadata.get('filenames', {}) %}
+<img src="{{ output.metadata.filenames['image/png'] | posix_path }}"
+{%- else %}
+<img src="data:image/png;base64,{{ output.data['image/png'] }}"
+{%- endif %}
+{%- set width=output | get_metadata('width', 'image/png') -%}
+{%- if width is not none %}
+width={{ width }}
+{%- endif %}
+{%- set height=output | get_metadata('height', 'image/png') -%}
+{%- if height is not none %}
+height={{ height }}
+{%- endif %}
+{%- if output | get_metadata('unconfined', 'image/png') %}
+class="unconfined"
+{%- endif %}
+ alt = 'png'>
+{%- endblock data_png %}
+
+{% block data_jpg scoped %}
+<div class="output_jpeg output_subarea {{ extra_class }}">
+{%- if 'image/jpeg' in output.metadata.get('filenames', {}) %}
+<img src="{{ output.metadata.filenames['image/jpeg'] | posix_path }}"
+{%- else %}
+<img src="data:image/jpeg;base64,{{ output.data['image/jpeg'] }}"
+{%- endif %}
+{%- set width=output | get_metadata('width', 'image/jpeg') -%}
+{%- if width is not none %}
+width={{ width }}
+{%- endif %}
+{%- set height=output | get_metadata('height', 'image/jpeg') -%}
+{%- if height is not none %}
+height={{ height }}
+{%- endif %}
+{%- if output | get_metadata('unconfined', 'image/jpeg') %}
+class="unconfined"
+{%- endif %}
+>
+</div>
+{%- endblock data_jpg %}
