81e7eeefd148856264a20dc0072c17e417638364
Tania Allard <t.allard@sheffield.ac.uk>
2018-01-04 09:46:11 +0000

    Add  date conversion function to utils ⏳

--- utils/nb_git.py
+++ utils/nb_git.py
@@ -27,27 +27,40 @@ class nb_repo(object):
 
     def check_log(self):
         """ Check the number of commits in the repository,
-        if there is only one commit it will find all the notebooks.
-        Otherwise it will find the notebooks in the latest commit only
+        if there is only one commit it will find all the notebooks
+        inside the repository.
+        Otherwise, it will find the notebooks in the latest commit only
+        Returns
+            -------
+            notebooks: dictionary containing the sha1 for the commit,
+            the list of found notebooks and the date when the notebooks
+            were last updated
         """
         all_commits = [commit for commit in self.repo.head.log()]
         if len(all_commits) <= 1:
             print('Only one commit: converting all notebooks')
+            # calls function find_notebooks
             nb_coll = self.find_notebooks()
             sha1 = self.repo.revparse_single('HEAD').hex[0:7]
+            author =
+
             notebooks = {'sha1': sha1,
                          'notebooks': nb_coll}
             return notebooks
         else:
             print(("There are notebooks already in version control,"
                    "finding the notebooks passed in the last commit"))
+            # calls function last_commit
             notebooks = self.last_commit()
             return notebooks
 
     def find_notebooks(self):
         """ Find all the notebooks in the repo, but excludes those
-        in the _site folder, this will be default if no specific
-        notebook was passed for conversion
+        in the _site folder.
+        Returns
+            -------
+            notebooks: dictionary containing the sha1, notebooks name,
+            and dat of the commit
         """
         basePath = Path(os.getcwd())
         notebooksAll = [nb for nb in glob.glob('**/*.ipynb')]
@@ -60,15 +73,46 @@ class nb_repo(object):
             return notebooks
 
     def last_commit(self):
-        last = self.repo.revparse_single('HEAD')
-        sha1 = last.hex[0:7]
+        """ Find the notebooks modified in the last repository, but excludes those
+                in the _site folder.
+                Returns
+                    -------
+                    notebooks: dictionary containing the sha1, notebooks name,
+                    and dat of the commit
+                    if no notebooks were modified in the last commit then
+                    the list notebooks is an empty list
+        """
+
+        nb_coll = get_commit()
         notebooks = [nb.name for nb in last.tree if '.ipynb' in nb.name]
-        nb_coll = {'sha1': sha1,
-                   'notebooks': notebooks}
+        nb_coll['notebooks'] = notebooks
+
         return nb_coll
 
-    def convert_time(self):
+    def convert_time(self, epoch):
+        """
+        Pass on the epoch date from the last commit and
+        returns it in a human readable format
+        :param epoch:
+        :return: commit date in a dd-mm-YYYY format
+        """
         time_zone = pytz.timezone('GMT')
-        dt = datetime.fromtimestamp(commit, time_zone)
-        date = dt.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+        dt = datetime.fromtimestamp(epoch, time_zone)
 
+        return dt.strftime('%d-%m-%Y')
+
+    def get_commit(self):
+        """
+        Get the information for the last commit in the repository
+        :return: dictionary with the sha1, date, and author
+        of the last commit.
+        """
+        last = self.repo.revparse_single('HEAD')
+        sha1 = last.hex[0:7]
+        author = last.author.name
+        date = convert_time(last.author.time)
+        commit_info = {'sha1': sha1,
+                   'date': date,
+                   'author': author}
+
+        return commit_info
