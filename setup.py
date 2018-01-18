7471dc6d383fbe9d0778d154182eeaf2377c8e6c
Tania Allard <t.allard@sheffield.ac.uk>
2018-01-18 13:06:49 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ pkg_root = os.path.join(os.path.dirname(__file__), name)
 here = os.path.dirname(__file__)
 
 setup_args = dict(name = name,
-                  version = '0.1',
+                  version = '0.1.1',
                   description = 'Package used for easy conversion from Jupyter notebook to Jekyll posts',
                   url = 'https://github.com/trallard/nbconvert-jekyllconvert.git',
-                  donwload_url ='https://github.com/trallard/nbconvert-jekyllconvert/archive/v0.1.tar.gz',
+                  donwload_url ='https://github.com/trallard/nbjekyll/archive/v0.1.1.tar.gz',
                   author = 'Tania Allard',
                   author_email = 't.allard@sheffield.ac.uk',
                   description_file = 'README.md',
@@ -20,7 +20,9 @@ setup_args = dict(name = name,
                   packages = find_packages(),
                   zip_safe = False,
                   install_requires = ['pygit2', 'nbval', 'nbconvert >= 5.0','pytz'],
-                  keywords = ['jekyll', 'nbconvert', 'jupyternotebooks'])
+                  keywords = ['jekyll', 'nbconvert', 'jupyternotebooks'],
+                  classifiers = ["Programming Language :: Python :: 3","Development Status :: 4 - Beta",
+                  "Intended Audience :: Education", "License :: OSI Approved :: MIT License" ] )
 
 if __name__ == '__main__':
     setup(**setup_args)
