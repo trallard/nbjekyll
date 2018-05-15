51743c2669802784d03530fead322c42de61ec03
trallard <t.allard@sheffield.ac.uk>
2018-05-15 11:40:02 +0100

    Update setup.py: add keywords and open long_description

--- setup.py
+++ setup.py
@@ -9,20 +9,44 @@ here = os.path.dirname(__file__)
 
 setup_args = dict(name = name,
                   version = '0.1.1',
+
                   description = 'Package used for easy conversion from Jupyter notebook to Jekyll posts',
+                  long_description = open('README.md').read(),
+
                   url = 'https://github.com/trallard/nbconvert-jekyllconvert.git',
                   donwload_url ='https://github.com/trallard/nbjekyll/archive/v0.1.1.tar.gz',
+
+                  # Author details
                   author = 'Tania Allard',
-                  author_email = 't.allard@sheffield.ac.uk',
-                  description_file = 'README.md',
+                  author_email = 'tania',
+
                   license = 'MIT',
                   include_package_data = True,
+
+                  # You can just specify the packages manually here if your project is
+                  # simple. Or you can use find_packages().
                   packages = find_packages(),
                   zip_safe = False,
+
                   install_requires = ['pygit2', 'nbval', 'nbconvert >= 5.0','pytz'],
-                  keywords = ['jekyll', 'nbconvert', 'jupyternotebooks'],
-                  classifiers = ["Programming Language :: Python :: 3","Development Status :: 4 - Beta",
-                  "Intended Audience :: Education", "License :: OSI Approved :: MIT License" ] )
+
+                  keywords =  'jupyter, jekyll, teaching, dissemination, open science'
+
+                  classifiers = [
+                    # Specify the Python versions you support here. In particular, ensure
+                    # that you indicate whether you support Python 2, Python 3 or both.
+                    'Programming Language :: Python :: 2',
+                    'Programming Language :: Python :: 2.6',
+                    'Programming Language :: Python :: 2.7',
+                    'Programming Language :: Python :: 3',
+                    'Programming Language :: Python :: 3.2',
+                    'Programming Language :: Python :: 3.3',
+                    'Programming Language :: Python :: 3.4',
+                    'Programming Language :: Python :: 3.5',
+                    'Programming Language :: Python :: 3.6',
+                    "Development Status :: 3 - Alpha",
+                    "Intended Audience :: Education",
+                    "License :: OSI Approved :: MIT License" ] )
 
 if __name__ == '__main__':
     setup(**setup_args)
